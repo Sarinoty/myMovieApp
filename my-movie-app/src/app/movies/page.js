3268754237f6ab4e2efbacdd537051cfc34710c1
Sarinoty <sarinoty@gmail.com>
2023-10-15 17:48:33 +0200

    Prepa1

--- my-movie-app/src/app/movies/page.js
+++ my-movie-app/src/app/movies/page.js
@@ -3,7 +3,11 @@ import React from 'react';
 const MoviesPage = () => {
     return (
         <div></div>
+<<<<<<< HEAD
+    )
+=======
     );
+>>>>>>> 52d0569a4284a2787c34ddda3af57b6f9dfcb051
 };
 
 export default MoviesPage;
--- my-movie-app/src/app/series/page.js
+++ my-movie-app/src/app/series/page.js
@@ -3,7 +3,11 @@ import React from 'react';
 const SeriesPage = () => {
     return (
         <div></div>
+<<<<<<< HEAD
+    )
+=======
     );
+>>>>>>> 52d0569a4284a2787c34ddda3af57b6f9dfcb051
 };
 
 export default SeriesPage;
