43a1ddef331c0b7e444ae05703dc6acbdd39844c
Sarinoty <sarinoty@gmail.com>
2024-02-23 17:24:34 +0100

    Commentaire sur .env

--- my-movie-app/src/components/popular/Popular.js
+++ my-movie-app/src/components/popular/Popular.js
@@ -5,11 +5,9 @@ import styles from "./Popular.module.scss";
 
 const Popular = async () => {
     const {results} = await getMovieByPath("/movie/popular"); // On récupère les films les plus populaires sur tmdb.
+    // Attention, au moment du build il faut un .env.local ou un .env.production ou .env.poduction.local pour que l'app ait accès aux variables d'environnement (voir cours)
     const popularMovies = results.slice(0, 6); // On ne garde que les 6 premiers résultats.
-    //console.log("Results : ");
-    //console.log(results);
-    //console.log("PopularMovies filtrées : ");
-    //console.log(popularMovies);
+
     return (
         <div>
             <h2>Les plus populaires</h2>
