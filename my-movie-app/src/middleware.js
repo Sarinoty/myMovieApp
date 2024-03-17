3123f43b110f527331681eb43273317ff36fa284
Sarinoty <sarinoty@gmail.com>
2024-03-17 12:02:16 +0100

    Correction erreurs

--- my-movie-app/src/middleware.js
+++ my-movie-app/src/middleware.js
@@ -1,11 +1,11 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { getLocaleUrlToRedirect } from "./utils/i18n";
 
 export function middleware(request) {
     const newLocaleUrl = getLocaleUrlToRedirect(request); // On interroge la fonction pour savoir s'il faut rediriger ou non.
 
     if (newLocaleUrl) { // Si on a reçu une url de 'getLocaleUrlToRedirect' c'est qu'il faut rediriger l'utilisateur.
-        return NextRequest.redirect(newLocaleUrl); // On redirige vers l'url reçue.
+        return NextResponse.redirect(newLocaleUrl); // On redirige vers l'url reçue.
     };
 };
 
--- my-movie-app/src/utils/i18n.js
+++ my-movie-app/src/utils/i18n.js
@@ -1,18 +1,18 @@
-import {match} from "@formatjs/intl-localematcher";
+import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
 export const availableLocales = ["en", "fr"]; // On défini les langues qu'on veut supporter.
 export const defaultLocale = "fr"; // On défini la langue a afficher si la langue de l'utilisateur n'est pas prise en charge.
 
 export const getPreferredLocale = (request) => { // On récupère la langue de l'utilisateur dans les headers de la requête.
-    const headers = {"accept-laguage": request.headers.get("accept-language")}; // On récupère les accept-language renvoyés par le navigateur.
+    const headers = { "accept-language": request.headers.get("accept-language") }; // On récupère les accept-language renvoyés par le navigateur.
     const languages = new Negotiator({headers}).languages(); // On récupère les langues par ordre de préférence.
     return match(languages, availableLocales, defaultLocale); // On récupère la langue à utiliser.
 };
 
 export const getLocaleUrlToRedirect = (request) => { // Cette fonction sera appelée par le middleware. Elle dira si oui ou non il y a besoin de rediriger.
     const pathname = request.nextUrl.pathname; // On récupère le pathname de la requête.
-    const pathnameIsMissingLocale = availableLocales.every((locale) => !pathname.startswith(`/${locale}`) && pathname !== `/${locale}`);
+    const pathnameIsMissingLocale = availableLocales.every((locale) => !pathname.startsWith(`/${locale}`) && pathname !== `/${locale}`);
     // On regarde toutes les langues qu'on supporte (locale) et on vérifie que l'url ne commence pas par '/${locale}' et que le pathname est différent '/${locale}'.
 
     if (pathnameIsMissingLocale) { // Si pathnameIsMissingLocale est non null c'est qu'il faut rediriger l'utilisateur vers sa langue.
